3d27e23fb4cdb978c056a1b1cc725e6e20a33a03
Armando Arismendi <armando.arismendi.23@gmail.com>
2023-09-27 16:37:41 -0300

    Funcionalidad iniciada (en proceso) - Seleccion de elementos favoritos

--- main.js
+++ main.js
@@ -1,112 +1,74 @@
 const URL = "https://api.thecatapi.com/v1/images/search";
-const API_Key = "live_aD81OlUwOrxOFyT4Pjjtcjmxe4pgty3jxEcZeoAQFoYp7ijqFGr8pMOyBc9ba0qR"
+const API_Key = "live_aD81OlUwOrxOFyT4Pjjtcjmxe4pgty3jxEcZeoAQFoYp7ijqFGr8pMOyBc9ba0qR";
 
-let src;
-
-/** 
- * Llamada al API - Multiples imagenes
- * @param {string} URL - Url get de la cual tomar la informacion
- * @param {number} amount - Cantidad de imagenes de gatos que desea desplegar 
- * Esta llamada funciona sin api key, recibiendo un array de 10 items los cuales se filtran dependiendo de cuantos se deseen;
- * La llamada se hace con promesas
-*/
-/*function severalImg(amount){
-    fetch(`${URL}?limit=${amount}&api_key=${API_Key}`)
-    .then(res => res.json())
-    .then(data => {
-
-        let urlPhoto = [];
-        let idItem = [];
-        let showPhotos = [];
-        let showPhotosFilter = [];
-        let removeUrl;
-
-        for(i = 0; i< data.length; i++){
-            urlPhoto.push(data[i].url)
-            idItem.push(data[i].id)
-        }
-        console.log(idItem)
-
-        while(showPhotos.length < amount){
-            removeUrl = Math.floor(Math.random()*urlPhoto.length)
-            showPhotos.push(urlPhoto[removeUrl]) ;
-            urlPhoto.slice(removeUrl)
-        }
-        
-        showPhotosFilter = showPhotos.filter((item,index)=>{
-            return showPhotos.indexOf(item) === index;
-        })
-
-        showPhotosFilter.forEach(element => {
-            let divElement = document.createElement("div")
-            let divInputFavorite = document.createElement("div")
-            divElement.className = "imgContainer"
-            divInputFavorite.className = "favorites-cats"
-            /*let divInputFavorite = `<div class="favorites-cats">
-                                        <i class="fa-regular fa-heart"></i>
-                                    </div>`
-
-            let img = document.createElement("img")
-            let container = document.getElementById("container")
-
-            divElement.appendChild(img).setAttribute("src",element)
-            divElement.appendChild(divInputFavorite);
-            divInputFavorite.innerHTML = `<i class="fa-solid fa-heart"></i>`
-            divInputFavorite.setAttribute("onmouseover", "iconChange()")
-            container.appendChild(divElement)
-        })
-    })
-}*/
-
-let amount = 20;
+let amount = 100;
+let imgArray = [];
 
 /** 
- * Llamada al API - Una imagen
+ * Llamada al API
  * @param {string} URL - Url get de la cual tomar la informacion
- * Esta llamada funciona sin api key, recibiendo un unico valor
- * La llamada se hace con asincronismo
+ * @param {number} amount - Cantidad de imagenes que se quiere traer
+ * Esta llamada funciona pasando una api key
 */
-
 async function call(amount){
     const res = await fetch(`${URL}?limit=${amount}&api_key=${API_Key}`);
     const data = await res.json();
 
-    let imgArray = [];
+    imgArray = [];
     let auxArray = [];
 
     for(let i = 0; i< data.length; i++){
-        auxArray.push(data[i].url)
-        auxArray.push(data[i].id)
-        imgArray.push(auxArray)
-        auxArray = []
+        auxArray.push(data[i].url);
+        auxArray.push(data[i].id);
+        imgArray.push(auxArray);
+        auxArray = [];
     }
 
     imgArray.forEach(element => {
-        let divElement = document.createElement("div")
-        let divInputFavorite = document.createElement("div")
-        divElement.className = "imgContainer"
-        divInputFavorite.className = "favorites-cats"
+        let divElement = document.createElement("div");
+        let divInputFavorite = document.createElement("div");
+        divElement.className = "imgContainer";
+        divInputFavorite.className = "favorites-cats";
 
-        let img = document.createElement("img")
+        let img = document.createElement("img");
         let container = document.getElementById("container");
 
         divElement.appendChild(img).setAttribute("src",element[0]);
         divElement.appendChild(divInputFavorite);
+        divInputFavorite.setAttribute("data-favorites", false);
+        divInputFavorite.setAttribute("id", element[1]);
+        divInputFavorite.setAttribute("onclick", "favoriteCats(this)");
         divInputFavorite.innerHTML = `<i class="fa-solid fa-heart"></i>`;
 
-        container.appendChild(divElement)
+        container.appendChild(divElement);
     })
 }
 
 call(amount);
 
 function reCall(){
-    let container = document.getElementById("container")
-    let removeElements = document.querySelectorAll("div.imgContainer")
-    //console.log(removeElements)
+    let container = document.getElementById("container");
+    let removeElements = document.querySelectorAll("div.imgContainer");
     removeElements.forEach(element => {
-        container.removeChild(element)
+        container.removeChild(element);
     })
-    //severalImg(100);
     call(amount);
+}
+
+function favoriteCats(e){
+    let idImgCat = e.getAttribute("id");
+
+    let clickElement = document.getElementById(idImgCat);
+    let auxIconSelector = clickElement.childNodes[0];
+
+    if(clickElement.getAttribute("data-favorites") === "false"){
+        clickElement.setAttribute("data-favorites", "true");
+        clickElement.className = "favorites-cats active-favorites-cats";
+        auxIconSelector.className = "fa-solid fa-heart active-favorites-cats-icon";
+    }else{
+        clickElement.setAttribute("data-favorites", "false");
+        clickElement.className = "favorites-cats";
+        auxIconSelector.className = "fa-solid fa-heart";
+    }
+    
 }
